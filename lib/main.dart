cb4f96de092add686b3212dc8dadd6a7956e8db8
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-09-27 15:23:39 +0500

    Extracting widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -22,28 +23,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(text: "text2", author: "author2")
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                quote.text,
-                style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
-              ),
-              const SizedBox(height: 6.0),
-              Text(
-                quote.author,
-                style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
-              )
-            ],
-          ),
-        ));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,9 +34,10 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         children: quotes
-            .map((quote) => quoteTemplate(quote))
+            .map((quote) => QuoteCard(quote))
             .toList(),
       ),
     );
   }
 }
+
